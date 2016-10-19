6ae6e658e3447a204148235209873c23eb6110a5
Laurhens D <drhens@gmail.com>
2016-10-19 11:06:10 -0400

    able to show place searched as an object

--- public/app.js
+++ public/app.js
@@ -41,32 +41,11 @@ function initMap() {
     marker.setVisible(true);
 
     infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-        'Place ID: ' + place.place_id + '<br>' +
-        place.formatted_address);
+      'Place ID: ' + place.place_id + '<br>' +
+      place.formatted_address);
     infowindow.open(map, marker);
+
+    console.log(place);
   });
 }
 
-/*
-
-TODO: how to access the json objects for id, name, website, rating
-  etc.
-
-*/
-
-// service.getDetails({
-//           placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
-//         }, function(place, status) {
-//           if (status === google.maps.places.PlacesServiceStatus.OK) {
-//             var marker = new google.maps.Marker({
-//               map: map,
-//               position: place.geometry.location
-//             });
-//             google.maps.event.addListener(marker, 'click', function() {
-//               infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-//                 'Place ID: ' + place.place_id + '<br>' +
-//                 place.formatted_address + '</div>');
-//               infowindow.open(map, this);
-//             });
-//           }
-//         });
